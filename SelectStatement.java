e873cd1390eb0f032942fa0281eea3b462b8f8e3
Vijay Ramakrishnan <vijay120@gmail.com>
2013-03-29 01:10:49 -0700

    finished with create database

--- SelectStatement.java
+++ SelectStatement.java
@@ -3,19 +3,12 @@ public class SelectStatement {
     protected String select;
     protected String from;
     protected String where;
-    protected String attrslist;
+    protected String database;
 
     public SelectStatement () {
         this.select = null;
         this.from = null;
         this.where = null;
-        this.attrslist = null;
-    }
-    
-    public SelectStatement (String select, String from, String where) {
-        this.select = select;
-        this.from = from;
-        this.where = where;
-    }
-    
+        this.database = null;
+    }    
 }
---
+++ test_input.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE bookstore;
+select name, major from Students where 1 = "DemasE.Candaele";
+select * from Courses where 3 = "Spring";
+select * from Students where 4 = 1;
+select * from Students where 1 = "DemasE.Candaele";
+select * from Students;
+select * from Students where 3 = "MiningEngineering";
+EXIT;
